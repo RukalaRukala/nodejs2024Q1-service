8510cc6f2004672fe793fc76ba04da7d7d9ec6d0
Mikalai Rusakovich <kolyos83@mail.ru>
2024-03-07 01:12:01 +0300

    feat: create dataBase and interfaces

---
+++ src/dataBase/db.ts
@@ -0,0 +1,45 @@
+import { IDataBase } from './dataBase.model';
+
+export const db: IDataBase = {
+  users: [
+    {
+      id: '1',
+      login: 'Rukala',
+      password: '123',
+      version: 1,
+      createdAt: 0,
+      updatedAt: 0,
+    },
+  ],
+  artists: [
+    {
+      id: '1',
+      name: 'Mikalai',
+      grammy: false,
+    },
+  ],
+  album: [
+    {
+      id: '1',
+      name: 'My life',
+      year: 1983,
+      artistId: '1',
+    },
+  ],
+  tracks: [
+    {
+      id: '1',
+      name: 'Hello',
+      artistId: '1',
+      albumId: '1',
+      duration: 1,
+    },
+  ],
+  favorites: [
+    {
+      artists: ['Sting', 'Pink', 'Madonna'],
+      tracks: ['Hello', 'Mellow', 'Yellow'],
+      albums: ['My life', 'His life', 'Her life'],
+    },
+  ],
+};
