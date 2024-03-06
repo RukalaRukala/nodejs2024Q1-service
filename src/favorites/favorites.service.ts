8766429a8ec8490d32696cb8eb2f0c55d0102783
Mikalai Rusakovich <kolyos83@mail.ru>
2024-03-07 02:17:28 +0300

    feat: create favorites module

---
+++ src/favorites/favorites.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateFavoriteDto } from './dto/create-favorite.dto';
+import { UpdateFavoriteDto } from './dto/update-favorite.dto';
+import { IFavorites } from '../dataBase/dataBase.model';
+import { db } from '../dataBase/db';
+
+@Injectable()
+export class FavoritesService {
+  private readonly favorites: IFavorites;
+
+  constructor() {
+    this.favorites = db.favorites;
+  }
+
+  create(createFavoriteDto: CreateFavoriteDto) {
+    return 'This action adds a new favorite';
+  }
+
+  findAll() {
+    return `This action returns all favorites`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} favorite`;
+  }
+
+  update(id: number, updateFavoriteDto: UpdateFavoriteDto) {
+    return `This action updates a #${id} favorite`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} favorite`;
+  }
+}
