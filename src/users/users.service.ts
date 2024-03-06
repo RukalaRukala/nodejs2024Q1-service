4ea4fd92915eff69047c71479859413f0b8f1e93
Mikalai Rusakovich <kolyos83@mail.ru>
2024-03-07 01:13:13 +0300

    feat: create user module

---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { IUser } from '../dataBase/dataBase.model';
+import { db } from '../dataBase/db';
+
+@Injectable()
+export class UsersService {
+  private readonly users: IUser[];
+
+  constructor() {
+    this.users = db.users;
+  }
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
