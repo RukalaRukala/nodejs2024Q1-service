24eaa989d4ebbb82333657f237bd2c6a632a7069
Mikalai Rusakovich <kolyos83@mail.ru>
2024-03-06 23:09:51 +0300

    feat: use .env file, add message of running server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
+
+export const color = {
+  blue: '\x1b[34m',
+  red: '\x1b[31m',
+  green: '\x1b[32m',
+  reset: '\x1b[0m'
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const port = process.env.PORT || 4000;
+  await app.listen(port, () => {
+    console.log(`\
+${color.green}\nServer running successfully on the port \
+${color.blue}http://localhost:4000/\
+${color.reset}`)
+  });
 }
 bootstrap();
