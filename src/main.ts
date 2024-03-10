b135ff41a32184a75d59d8f68f09d4515b80c033
Mikalai Rusakovich <kolyos83@mail.ru>
2024-03-10 11:24:51 +0300

    refactor: separate start server message function

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { startServerMessage } from './startServerMessage/startServerMessage';
 config();
-
-export const color = {
-  blue: '\x1b[34m',
-  red: '\x1b[31m',
-  green: '\x1b[32m',
-  reset: '\x1b[0m',
-};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 4000;
-  await app.listen(port, () => {
-    console.log(`\
-${color.green}\nServer running successfully on the port \
-${color.blue}http://localhost:4000/\
-${color.reset}`);
-  });
+  await app.listen(process.env.PORT || 4000, startServerMessage);
 }
 bootstrap();
