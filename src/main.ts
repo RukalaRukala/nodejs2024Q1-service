31edddfabbadd65173014ed47fac15e9402c1b43
Mikalai Rusakovich <kolyos83@mail.ru>
2024-03-11 14:29:36 +0300

    feat: add swagger and change doc.yaml

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { startServerMessage } from './startServerMessage/startServerMessage';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const file = await readFile('doc/api.yaml', 'utf-8');
+  const swaggerDocument = parse(file);
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   await app.listen(process.env.PORT || 4000, startServerMessage);
 }
 bootstrap();
