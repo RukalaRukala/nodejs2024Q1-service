72b9e7d0a843b6d9cbf8f61bffa4e91ddf60b23d
Mikalai Rusakovich <kolyos83@mail.ru>
2024-03-16 18:56:10 +0300

    feat: create prisma seed.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import {PrismaClient} from '@prisma/client';
+import {v4 as uuidv4} from "uuid";
+import {db} from "../src/dataBase/db";
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+    // create two dummy articles
+    const user = await prisma.user.create({
+            data: {
+                id: uuidv4(),
+                login: 'SuperPupers',
+                password: '12345',
+                version: 1,
+            }
+        }
+    );
+    const userFromDB = await prisma.user.findMany();
+
+    console.log({user, userFromDB}, '\n', db);
+}
+
+// execute the main function
+main()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        // close Prisma Client at the end
+        await prisma.$disconnect();
+    });
